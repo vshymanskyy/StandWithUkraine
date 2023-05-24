639b9e6a18b04750fc5ebbcde750a9e113cabfed
Julian Wandhoven <Julian.wandhoven@gmail.com>
2023-05-24 23:28:24 +0200

    updated war

--- react/src/components/SaveUkraineBanScreen/SaveUkraineBanScreen.tsx
+++ react/src/components/SaveUkraineBanScreen/SaveUkraineBanScreen.tsx
@@ -9,15 +9,17 @@ export const UKRAINE_FLAG_IMAGE_URL = "data:image/svg+xml,%3Csvg class='flag' xm
 export interface SaveUkraineBanScreenProps extends React.HTMLProps<HTMLDivElement> {
   text?: JSX.Element;
   hideBlood?: boolean;
+  isGraphicIncluded?: boolean | string;
 }
 
 export const defaultProps = {
   text: <>Остановить войну с <b>Украиной</b></>,
   hideBlood: false,
+  isGraphicIncluded: true,
 }
 
 export default function SaveUkraineBanScreen(props: SaveUkraineBanScreenProps) {
-  const { text, hideBlood, className, onClick, ...other } = { ...defaultProps, ...props };
+  const { text, hideBlood, isGraphicIncluded, className, onClick, ...other } = { ...defaultProps, ...props };
 
   return <div className={ "SaveUkraineBanScreen " + className } {...other}>
     <Flag/>
@@ -30,15 +32,21 @@ export default function SaveUkraineBanScreen(props: SaveUkraineBanScreenProps) {
       :
         null
     }
+    {
+      isGraphicIncluded ?
+        <img src={getGraphicUrl(isGraphicIncluded)} className="graphics"/>
+      :
+      null
+    }
   </div>
 }
 
-function getGraphicUrl(isGraphicIncluded: string) {
+function getGraphicUrl(isGraphicIncluded: string | boolean) {
   if (typeof isGraphicIncluded === 'string') {
       return isGraphicIncluded;
   } else {
       return randomItem(
-          `https://raw.githubusercontent.com/JulianWww/StandWithUkraine/main/assets/war1.jpg`,
+          `https://raw.githubusercontent.com/JulianWww/StandWithUkraine/main/assets/graphics/war1.jpg`,
           // TODO: More images @see https://facebook.com/story.php?story_fbid=10221678909548124&id=1416254989&m_entstream_source=timeline
       );
   }
